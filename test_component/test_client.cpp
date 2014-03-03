d1307d141abc8cb70c4ed4ce69895047c8985f11
John Biddiscombe <biddisco@cscs.ch>
2014-03-03 20:36:54 +0100

    Add barriers to test to prevent segfaults
    One barrier after object creation to ensure objects are
    created on all ranks before attempting to use them.
    
    Before shut down, add a barrier to ensure all actions have returned
    prior to calling finalize.

--- test_component/test_client.cpp
+++ test_component/test_client.cpp
@@ -6,38 +6,84 @@
 #include "test_component.hpp"
 #include <hpx/hpx_init.hpp>
 #include <hpx/runtime/agas/interface.hpp>
+#include <hpx/lcos/barrier.hpp>
 #include <boost/lexical_cast.hpp>
+
+static hpx::lcos::barrier unique_barrier;
+
+// Create a new barrier and register its gid with the given symbolic name.
+inline hpx::lcos::barrier
+create_barrier(std::size_t num_localities, char const* symname)
+{
+    hpx::lcos::barrier b;    
+    std::cout << "Creating barrier based on N localities " << num_localities << std::endl;
+    b.create(hpx::find_here(), num_localities);
+    hpx::agas::register_name_sync(symname, b.get_gid());
+    return b;
+}
+
+// Find a registered barrier object from its symbolic name.
+inline hpx::lcos::barrier
+find_barrier(char const* symname)
+{
+    hpx::id_type id;
+    for (std::size_t i = 0; i != HPX_MAX_NETWORK_RETRIES; ++i)
+    {
+        hpx::error_code ec;
+        id = hpx::agas::resolve_name_sync(symname, ec);
+        if (!ec) break;
+
+        boost::thread::sleep( boost::get_system_time() +
+            boost::posix_time::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
+    }
+    return hpx::lcos::barrier(id);
+}
+
+void create_barrier_startup()
+{
+  unique_barrier = create_barrier(hpx::find_all_localities().size(), "/my_startup_barrier");
+}
 
 int hpx_main(boost::program_options::variables_map&)
 {
-  {
-    hpx::naming::id_type here = hpx::find_here();
-    uint32_t          hpxrank = hpx::naming::get_locality_id_from_id(here);
-    std::cout << "hpx_main running on locality " << hpxrank << std::endl;
-    //
-    std::cout << "registering component with global name " << "/component/" 
-      << boost::lexical_cast<std::string>(hpxrank).c_str() << std::endl;
-    //
-    auto object = hpx::components::new_<server::test_component>(here).get();
-    hpx::agas::register_name_sync(
-      "/component/" + boost::lexical_cast<std::string>(hpxrank), 
-      object);
+  hpx::naming::id_type here = hpx::find_here();
+  uint32_t          hpxrank = hpx::naming::get_locality_id_from_id(here);
+  std::cout << "hpx_main running on locality " << hpxrank << std::endl;
+  //
+  std::cout << "registering component with global name " << "/component/" 
+    << boost::lexical_cast<std::string>(hpxrank).c_str() << std::endl;
+  //
+  auto object = hpx::components::new_<server::test_component>(here).get();
+  hpx::agas::register_name_sync(
+    "/component/" + boost::lexical_cast<std::string>(hpxrank), 
+    object);
 
-    // for each locality, find the handle of the component assigned to it
-    // and store it for later use.
-    std::vector<hpx::id_type> localities = hpx::find_all_localities();
-    std::vector<hpx::id_type> objects;
-    for (hpx::naming::id_type &l : localities) {
-      objects.push_back(
-        hpx::agas::resolve_name_sync("/component/" + boost::lexical_cast<std::string>(hpx::naming::get_locality_id_from_id(l)))
-        );
-    }
+  // wait until all processes have created their components
+  std::cout << "Waiting at barrier on " << hpxrank << std::endl; 
+  hpx::lcos::barrier b = find_barrier("/my_startup_barrier");
+  b.wait();
 
-    // for each object, invoke the test action
-    for (hpx::naming::id_type &o : objects) {
-      hpx::apply<server::test_component::invokeFrom_action>(o, hpxrank);
-    }
+  // for each locality, find the handle of the component assigned to it
+  // and store it for later use.
+  std::vector<hpx::id_type> localities = hpx::find_all_localities();
+  std::vector<hpx::id_type> objects;
+  for (hpx::naming::id_type &l : localities) {
+    objects.push_back(
+      hpx::agas::resolve_name_sync("/component/" + boost::lexical_cast<std::string>(hpx::naming::get_locality_id_from_id(l)))
+      );
   }
+
+  // for each object, invoke the test action
+  for (hpx::naming::id_type &o : objects) {
+    hpx::apply<server::test_component::invokeFrom_action>(o, hpxrank);
+  }
+
+  std::cout << "Waiting at barrier on " << hpxrank << std::endl; 
+//  hpx::lcos::barrier b = find_barrier("/my_startup_barrier");
+  b.wait();
+
+  // we don't need the barrier any more
+  hpx::agas::unregister_name_sync("/my_startup_barrier");
 
   return hpx::finalize(); // Initiate shutdown of the runtime system.
 }
@@ -47,6 +93,8 @@ int main(int argc, char* argv[])
   // Configure application-specific options.
   boost::program_options::options_description desc_commandline(
     "usage: " HPX_APPLICATION_STRING " [options]");
+
+  hpx::register_startup_function(&create_barrier_startup);
 
   return hpx::init(desc_commandline, argc, argv); 
 }
